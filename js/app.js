1b6aead9f6bc869a5741fc693ae4140e97263b0b
JessyCuevas <jessy.cuevas.aranda@gmail.com>
2018-01-22 11:37:21 -0600

    Web-App

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//*Splash*//
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".content").fadeIn(1500).fadeOut(1500);
+    },1000);
+  
+  setTimeout(function() {
+        $(".content2").fadeIn(1500);
+    },3000);
+});
